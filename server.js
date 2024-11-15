959c9b933c9efb51ec54adda1d7ea45e92a9d645
BritanyLM <britanylm@gmail.com>
2024-11-15 17:28:48 -0500

    first commit

--- server.js
+++ server.js
@@ -2,31 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello employees!");
-});
+app.use(express.json());
 
-const employees = require("./employees");
+const employeesRouter = require("./employeesRouter");
 
-app.get("/employees", (req, res) => {
-  res.json(employees);
-});
+app.use("/employees", employeesRouter);
 
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    res.status(404).send(`There is no employee with id ${id}.`);
-  }
+app.use((req, res, next) => {
+  res.status(404).send("Not found");
 });
 
 app.listen(PORT, () => {
   `Listening on port ${PORT}...`;
 });
+
